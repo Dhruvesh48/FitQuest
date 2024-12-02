42de998cfa5b0d8d242ebfb6aefab5b40d69b148
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-02 02:49:05 +0000

    added sorting

--- shop/views.py
+++ shop/views.py
@@ -10,6 +10,7 @@ def product_list(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort_by = request.GET.get('sort')
 
     if request.GET:
         if 'category' in request.GET:
@@ -25,11 +26,21 @@ def product_list(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+        if sort_by:
+            if sort_by == 'price_asc':
+                products = products.order_by('price')
+            elif sort_by == 'price_desc':
+                products = products.order_by('-price')
+            elif sort_by == 'rating_asc':
+                products = products.order_by('rating')
+            elif sort_by == 'rating_desc':
+                products = products.order_by('-rating')
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': sort_by,
     }
 
     return render(request, 'shop/products_list.html', context)
